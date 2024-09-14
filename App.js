4a3f6b2a171815d74f99a96611cdc773af778089
Farshid Ahmadi <ahmadi.fd@gmail.com>
2024-09-14 08:11:44 -0700

    ee

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
 import MessagesScreen from "./app/screens/MessagesScreen";
-import { List, TextInput } from "react-native-paper";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListItem from "./app/components/ListItem";
 import { Avatar } from "react-native-paper";
@@ -13,6 +12,8 @@ import { useState } from "react";
 import { Picker } from "@react-native-picker/picker";
 import LoginScreen from "./app/screens/LoginScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+
+
 
 export default function App() {
   console.log("Hello Farshid Ahmadi");
